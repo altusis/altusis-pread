29365abd49d9a9ac57e563072bc85c94231fbcfe
Sadaf Noor <sadaf2605@gmail.com>
2016-08-17 03:57:59 +0600

    basic redis pub sub with socket added

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 
+var redis = require("redis"),
+    client = redis.createClient();
+
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+
+
 /*
 io.on('connection', function(socket){
 	console.log("hello from 1")
@@ -12,10 +22,36 @@ io.on('connection', function(socket){
 });
 */
 app.get('/', function(req, res){
-  io.emit('course CSE111', 100);
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/users/courses', function(req, res){
+	res.json(["CSE111","CSE110"]);
+});
+
+
+app.get('/track/courses', function(req, res){
+	//loop
+	client.subscribe("CSE111");
+});
+
+app.post('/courses/follow', function(req, res){
+	console.log(req.body)
+	client.subscribe(req.body.course);
+});
+
+
 http.listen(3000, function(){
   console.log('listening on *:3000');
+});
+
+
+
+
+
+
+
+client.on("message", function(channel, message){
+  console.log(channel + ": " + message);
+  io.emit('course '+channel, message);
 });
